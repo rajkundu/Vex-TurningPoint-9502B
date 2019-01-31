c81c8b8065d68ac95af8e9ad6702d117d21e443b
rajkundu <rjk368@gmail.com>
2019-01-30 22:25:45 -0500

    updated function comments

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -15,22 +15,24 @@ extern Motor backRight;
 /**
  * sets drivetrain velocity
  * @param y desired forward-backward component
- *  range [-200, 200]
- *  units RPM
+ *  - range [-200, 200]
+ *  - units RPM
  * @param r desired rotational component
- *  range [-200, 200]
- *  units RPM
+ *  - range [-200, 200]
+ *  - units RPM
+ * @param scalingEnabled whether or not to apply proportional scaling to y and r
+ *  - default true
  */
 void driveRPM(int y, int r, bool scalingEnabled = true);
 
 /**
  * sets drivetrain speed using voltage control
  * @param y desired forward-backward component
- *  range [-127, 127]
- *  units N/A
+ *  - range [-127, 127]
  * @param r desired rotational component
- *  range [-127, 127]
- *  units N/A
+ *  - range [-127, 127]
+ * @param scalingEnabled whether or not to apply proportional scaling to y and r
+ *  - default true
  */
 void driveVoltage(int y, int r, bool scalingEnabled = true);
 
