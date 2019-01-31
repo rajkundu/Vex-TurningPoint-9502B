208d4cb2ef8c7d3b5f2017f051420ecda9bf8c6e
rajkundu <rjk368@gmail.com>
2019-01-30 21:33:18 -0500

    added "driveVoltage" function and clarified comments

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -21,6 +21,17 @@ extern Motor backRight;
  *  range [-200, 200]
  *  units RPM
  */
-void driveRPM(int y, int r);
+void driveRPM(int y, int r, bool scalingEnabled = true);
+
+/**
+ * sets drivetrain speed using voltage control
+ * @param y desired forward-backward component
+ *  range [-127, 127]
+ *  units N/A
+ * @param r desired rotational component
+ *  range [-127, 127]
+ *  units N/A
+ */
+void driveVoltage(int y, int r, bool scalingEnabled = true);
 
 //----------------------------------------------------------------------------//
--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -12,10 +12,10 @@ Motor backRight(4, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
 
 //--------- Functions --------//
 
-void driveRPM(int y, int r, bool scalingEnabled = true)
+void driveRPM(int y, int r, bool scalingEnabled)
 {
     //If the values are greater than 200, scale them proportionally so that they
-    //are within the motor bounds of 200
+    //are within the motor velocity bounds of [-200, 200]
     if(scalingEnabled)
     {
         if(abs(y) + abs(r) > 200)
@@ -27,6 +27,26 @@ void driveRPM(int y, int r, bool scalingEnabled = true)
 
     frontLeft.move_velocity(y + r);
     frontRight.move_velocity(-y + r);
+    backLeft.move_velocity(y + r);
+    backRight.move_velocity(-y + r);
+}
+
+void driveVoltage(int y, int r, bool scalingEnabled = true)
+{
+    //If the input values are greater than 127, scale them proportionally so
+    //that they are within the motor voltage bounds of [-12000, 12000]
+    if(scalingEnabled)
+    {
+        if(abs(y) + abs(r) > 127)
+        {
+            //12000 because in mV, not RPM
+            y = 12000 * y / ((float)(abs(y) + abs(r)));
+            r = 12000 * r / ((float)(abs(y) + abs(r)));
+        }
+    }
+
+    frontLeft.move_voltage(y + r);
+    frontRight.move_voltage(-y + r);
     backLeft.move_velocity(y + r);
     backRight.move_velocity(-y + r);
 }
