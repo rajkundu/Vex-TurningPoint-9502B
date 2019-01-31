6dfdba87eefe69c5385daa361b938df2ffd74fbb
rajkundu <rjk368@gmail.com>
2019-01-30 21:22:31 -0500

    added drivetrain velocity functions

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -1,2 +1,26 @@
 //Guard
 #pragma once
+
+//-------------------------------- Drivetrain --------------------------------//
+
+//---------- Motors ----------//
+
+extern Motor frontLeft;
+extern Motor frontRight;
+extern Motor backLeft;
+extern Motor backRight;
+
+//--------- Functions --------//
+
+/**
+ * sets drivetrain velocity
+ * @param y desired forward-backward component
+ *  range [-200, 200]
+ *  units RPM
+ * @param r desired rotational component
+ *  range [-200, 200]
+ *  units RPM
+ */
+void driveRPM(int y, int r);
+
+//----------------------------------------------------------------------------//
--- src/subsystems.cpp
+++ src/subsystems.cpp
@@ -1,2 +1,34 @@
 #include "main.h"
 #include "subsystems.hpp"
+
+//-------------------------------- Drivetrain --------------------------------//
+
+//---------- Motors ----------//
+
+Motor frontLeft(1, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+Motor frontRight(2, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+Motor backLeft(3, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+Motor backRight(4, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+
+//--------- Functions --------//
+
+void driveRPM(int y, int r, bool scalingEnabled = true)
+{
+    //If the values are greater than 200, scale them proportionally so that they
+    //are within the motor bounds of 200
+    if(scalingEnabled)
+    {
+        if(abs(y) + abs(r) > 200)
+        {
+            y = 200 * y / ((float)(abs(y) + abs(r)));
+            r = 200 * r / ((float)(abs(y) + abs(r)));
+        }
+    }
+
+    frontLeft.move_velocity(y + r);
+    frontRight.move_velocity(-y + r);
+    backLeft.move_velocity(y + r);
+    backRight.move_velocity(-y + r);
+}
+
+//----------------------------------------------------------------------------//
